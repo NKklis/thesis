a4c4b366496d3d2a4221d159547d60560d9acdad
zutoochi <k.yairi.guitar@icloud.com>
2024-05-22 17:50:02 +0900

    re

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+// Header.js 
+
+import React from 'react';
+import '../Header.css'; 
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-container">
+      <img src="/logoR.png" alt="PICTOR" className="header-logo" />
+        <nav className="header-nav">
+          <ul className="nav-list">
+            <li className="nav-item"><a href="#about">About</a></li>
+            <li className="nav-item"><a href="#Learn">Learn</a></li>
+            <li className="nav-item"><a href="#Community">Community</a></li>
+            <li className="nav-item"><a href="#Contact">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
